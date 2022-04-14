67f43e536f1a4f5a0702c02bbdd0e9a35f8d3ce6
jion-kozono <zeon66190329@gmail.com>
2022-04-14 17:52:13 +0900

    meaningをオプショナルに

--- schemas/word.py
+++ schemas/word.py
@@ -1,5 +1,5 @@
 from gettext import translation
-from typing import Literal
+from typing import Literal, Optional
 from pydantic import BaseModel, Field
 
 IsCorrect = Literal[1, -1]
@@ -7,9 +7,9 @@ IsCorrect = Literal[1, -1]
 class Word(BaseModel):
     id: str
     word: str
-    meaning: str
     example: str
     translation: str
+    meaning: Optional[str]
     book_name: str = Field("Basic Words 早稲田アカデミー")
     word_num: int
     is_correct: IsCorrect
